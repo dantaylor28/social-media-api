a2054fd99a5d6dd5ed6835cbd25a4d69a14d6f01
Dan <dan.taylor1493@gmail.com>
2025-02-13 06:43:47 +0100

    Create post model & meta class

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, blank=True)
+    caption = models.TextField(blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    post_image = models.ImageField(
+        upload_to='images/', default='../placeholder_post_pic_x9ygad', blank=True
+    )
+
+    class Meta:
+        ordering = ['-uploaded_at']
+
+    def __str__(self):
+        return f"post title: {self.title} by: {self.owner}"
+    
