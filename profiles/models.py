c0132fa1d92a8b2abae94e7b4951aab678d8c4e5
Dan <dan.taylor1493@gmail.com>
2025-02-13 08:18:03 +0100

    Create profile model and meta class

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, blank=True)
+    location = models.CharField(max_length=200, blank=True)
+    bio = models.TextField(blank=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    profile_image = models.ImageField(
+        upload_to='images/', default='../placeholder_profile_pic_z5brab'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile | last updated: {self.updated_at}"
