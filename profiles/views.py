498a9d89eaccb989986e342b9237012c050b4d87
Dan <dan.taylor1493@gmail.com>
2025-02-14 08:33:07 +0100

    Create ProfileDetailView to retrieve a single users profile

--- profiles/views.py
+++ profiles/views.py
@@ -6,3 +6,7 @@ from rest_framework import generics
 class ProfileListView(generics.ListAPIView):
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all().order_by('-created_at')
+
+class ProfileDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
