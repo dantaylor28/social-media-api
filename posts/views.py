c2fada69a0612daf96079596a641bf4e9faca4b0
Dan <dan.taylor1493@gmail.com>
2025-02-14 08:44:02 +0100

    Add docstrings to all of my view classes

--- posts/views.py
+++ posts/views.py
@@ -4,6 +4,10 @@ from .serializers import PostSerializer
 from rest_framework import generics
 
 class PostListView(generics.ListCreateAPIView):
+    """
+    Lists out all posts and the ability to create your own
+    if you are signed in and authenticated
+    """
     serializer_class = PostSerializer
     queryset = Post.objects.all().order_by('-uploaded_at')
 
@@ -11,5 +15,9 @@ class PostListView(generics.ListCreateAPIView):
         serializer.save(owner=self.request.user)
 
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Lists a selected post with the ability to edit or delete it
+    if you are the post owner
+    """
     serializer_class = PostSerializer
     queryset = Post.objects.all()
--- profiles/views.py
+++ profiles/views.py
@@ -4,9 +4,16 @@ from .serializers import ProfileSerializer
 from rest_framework import generics
 
 class ProfileListView(generics.ListAPIView):
+    """
+    Lists out all users and their profile information
+    """
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all().order_by('-created_at')
 
 class ProfileDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Lists all information for a selected user and the ability to update
+    & delete the profile if you are the owner
+    """
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
