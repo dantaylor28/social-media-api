36aacee4d76881fff2163d4d297e920f6f4c5e4e
Dan <dan.taylor1493@gmail.com>
2025-02-18 18:30:22 +0100

    Create FollowerSerializer with meta and fields

---
+++ followers/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Follower
+
+class FollowerSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Follower
+        fields = [
+            'id', 'owner', 'followed',
+            'created_at'
+        ]
