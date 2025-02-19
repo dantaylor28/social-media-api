bba827ea7fe82c0f4d83968e399b2d6c93ae2acb
Dan <dan.taylor1493@gmail.com>
2025-02-19 07:55:46 +0100

    Define a create method in FollowerSerializer to handle duplicate Follow attempts

--- followers/serializers.py
+++ followers/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Follower
+from django.db import IntegrityError
 
 class FollowerSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
@@ -11,3 +12,11 @@ class FollowerSerializer(serializers.ModelSerializer):
             'id', 'owner', 'followed',
             'followed_user', 'timestamp'
         ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'error': 'error'
+            })
