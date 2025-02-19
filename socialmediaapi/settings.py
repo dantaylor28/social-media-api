f0e5a54659e7f43e26dc06fbbf31e6d5069ebfcb
Dan <dan.taylor1493@gmail.com>
2025-02-19 08:22:20 +0100

    Add project wide default permission class, IsAuthenticatedOrReadOnly to my settings.py file

--- socialmediaapi/settings.py
+++ socialmediaapi/settings.py
@@ -15,6 +15,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ]
+}
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
