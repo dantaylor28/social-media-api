a7c56d9927e6cae0af638714a5f5dc8f61d29c5c
Dan <dan.taylor1493@gmail.com>
2025-02-20 08:26:11 +0100

    Fix error in commentLikeDetail url path

--- comment_likes/urls.py
+++ comment_likes/urls.py
@@ -3,5 +3,5 @@ from comment_likes import views
 
 urlpatterns = [
     path('comments/likes/', views.CommentLikeListView.as_view()),
-    path('comments/likes/int:pk', views.CommentLikeDetailView.as_view())
+    path('comments/likes/<int:pk>', views.CommentLikeDetailView.as_view())
 ]
