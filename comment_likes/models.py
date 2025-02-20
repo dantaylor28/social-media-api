57b1bd33f5c36920f36f1dad04be11b31c3808c7
Dan <dan.taylor1493@gmail.com>
2025-02-20 07:53:32 +0100

    Create CommentLike model and migrate changes to database

--- comment_likes/models.py
+++ comment_likes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from comments.models import Comment
 
-# Create your models here.
+class CommentLike(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(
+        Comment, on_delete=models.CASCADE, related_name='comment_likes')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-timestamp']
+        unique_together = ['owner', 'comment']
+
+    def __str__(self):
+        return f"{self.comment} liked by {self.owner}"
