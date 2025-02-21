e9a01918e1d0b59490b825ffea187b5f314df65e
Dan <dan.taylor1493@gmail.com>
2025-02-21 21:46:42 +0100

    Add docstring to my Category model

--- categories/models.py
+++ categories/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 class Category(models.Model):
+    """
+    Category model will store different category names in the
+    database. A category field is added to the Post model via a
+    foreign key to allow posts to be assigned a category upon creation
+    """
     name = models.CharField(max_length=100, unique=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
