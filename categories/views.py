47e249dda2a2599353427c6c8e9105a57f14c49c
Dan <dan.taylor1493@gmail.com>
2025-02-22 07:13:54 +0100

    Create custom permission isAdminOrReadOnly and apply it to categories/views.py

--- categories/views.py
+++ categories/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from .models import Category
 from .serializers import CategorySerializer
 from rest_framework import generics
-from socialmediaapi.permissions import IsOwnerOrReadOnly
+from socialmediaapi.permissions import IsAdminOrReadOnly
 
 class CategoryListView(generics.ListCreateAPIView):
     serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
     queryset = Category.objects.all()
 
 class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
     queryset = Category.objects.all()
