5ca5d509dff17441ac47a14658afcd40bb8b0264
Dan <dan.taylor1493@gmail.com>
2025-02-22 06:59:38 +0100

    Wire up the urls for the categories app

--- socialmediaapi/urls.py
+++ socialmediaapi/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('', include('comments.urls')),
     path('', include('followers.urls')),
     path('', include('comment_likes.urls')),
+    path('', include('categories.urls')),
 ]
