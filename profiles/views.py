c0677a2bca9e42734d772b5dd794d038ee5a5f9b
Dan <dan.taylor1493@gmail.com>
2025-02-26 07:30:27 +0100

    Use count to add num_of posts, pinned posts, followers and following to ProfileList and ProfileSerializer

--- profiles/views.py
+++ profiles/views.py
@@ -3,13 +3,19 @@ from .models import Profile
 from .serializers import ProfileSerializer
 from socialmediaapi.permissions import IsOwnerOrReadOnly
 from rest_framework import generics
+from django.db.models import Count
 
 class ProfileListView(generics.ListAPIView):
     """
     Lists out all users and their profile information
     """
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all().order_by('-created_at')
+    queryset = Profile.objects.annotate(
+        num_of_posts=Count('owner__post', distinct=True),
+        num_of_followers=Count('owner__followed', distinct=True),
+        num_of_following=Count('owner__following', distinct=True),
+        num_of_pinned_posts=Count('owner__pin', distinct=True)
+    ).order_by('-created_at')
 
 class ProfileDetailView(generics.RetrieveUpdateDestroyAPIView):
     """
