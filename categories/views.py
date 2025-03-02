4c599c232dcc5dda7c8855d7964567446de79af7
Dan <dan.taylor1493@gmail.com>
2025-03-02 09:44:09 +0100

    Add search and ordering to categories to search for a specific category or order by timestamp

--- categories/views.py
+++ categories/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.db.models import Count
 from .models import Category
 from .serializers import CategorySerializer
-from rest_framework import generics
+from rest_framework import generics, filters
 from socialmediaapi.permissions import IsAdminOrReadOnly
 
 class CategoryListView(generics.ListCreateAPIView):
@@ -11,6 +11,14 @@ class CategoryListView(generics.ListCreateAPIView):
     queryset = Category.objects.annotate(
         num_of_posts=Count('post', distinct=True)
     )
+
+    filter_backends = [
+        filters.SearchFilter,
+        filters.OrderingFilter
+    ]
+
+    search_fields = ['name']
+    ordering_fields = ['timestamp']
 
 class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = CategorySerializer
