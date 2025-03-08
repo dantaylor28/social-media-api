439cfefc922d020c3ca9d10209635522f29cd37a
Dan <dan.taylor1493@gmail.com>
2025-03-08 17:14:36 +0100

    Add automated tests for Followers ListView & DetailView

--- followers/tests.py
+++ followers/tests.py
@@ -1,3 +1,52 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Follower
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class FollowerListViewTests(APITestCase):
+    def setUp(self):
+        dan = User.objects.create_user(username='dan', password='password1')
+        sabina = User.objects.create_user(
+            username='sabina', password='password1')
+
+    def test_list_all_followers(self):
+        dan = User.objects.get(username='dan')
+        sabina = User.objects.get(username='sabina')
+        follow = Follower.objects.create(owner=dan, followed=sabina)
+        response = self.client.get('/followers/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logged_out_cannot_follow(self):
+        dan = User.objects.get(username='dan')
+        sabina = User.objects.get(username='sabina')
+        follow = Follower.objects.create(owner=dan, followed=sabina)
+        response = self.client.post('/followers/', {'followed': 'follow'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class FollowerDetailViewTests(APITestCase):
+    def setUp(self):
+        dan = User.objects.create_user(username='dan', password='password1')
+        sabina = User.objects.create_user(
+            username='sabina', password='password1')
+        follow1 = Follower.objects.create(owner=dan, followed=sabina)
+        follow2 = Follower.objects.create(owner=sabina, followed=dan)
+
+    def test_get_follow_by_id(self):
+        response = self.client.get('/followers/1')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_non_existant_follow_id(self):
+        response = self.client.get('followers/32')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_own_follow(self):
+        self.client.login(username='dan', password='password1')
+        response = self.client.delete('/followers/1')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_others_follow(self):
+        self.client.login(username='dan', password='password1')
+        response = self.client.delete('/followers/2')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
