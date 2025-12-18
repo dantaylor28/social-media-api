b517f72498a665101d31ecda4794e1b349fecd23
Dan <dan.taylor1493@gmail.com>
2025-12-18 21:36:30 +0100

    Replace image field in CurrentUserSerializer with a serializer method field to get correct img url

--- socialmediaapi/serializers.py
+++ socialmediaapi/serializers.py
@@ -5,11 +5,13 @@ User = get_user_model()
 
 class CurrentUserSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
-    profile_image = serializers.ImageField(
-        source='profile.profile_image',
-        read_only=True
-    )
+    profile_image = serializers.SerializerMethodField()
 
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'first_name', 'last_name', 'profile_id', 'profile_image')
+
+    def get_profile_image(self, obj):
+        if obj.profile.profile_image:
+            return obj.profile.profile_image.url
+        return None
