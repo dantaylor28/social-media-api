880ea0966182ba9df818918988643eccbaca6dd3
Dan <dan.taylor1493@gmail.com>
2026-06-07 09:44:35 +0200

    Attempt fix to Post display error

--- posts/views.py
+++ posts/views.py
@@ -30,7 +30,7 @@ class PostListView(generics.ListCreateAPIView):
     search_fields = [
         'title',
         'caption',
-        'tag__name',
+        # 'tag__name',
         'owner__username',
         'owner__profile__name'
     ]
@@ -43,7 +43,7 @@ class PostListView(generics.ListCreateAPIView):
 
     filterset_fields = [
         'owner__profile',
-        'tag__name',
+        # 'tag__name',
         # will show posts from users the selected user is following
         'owner__followed__owner__profile',
         # will show posts the selected user has pinned
